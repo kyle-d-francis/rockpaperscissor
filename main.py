2e057a2517b6a12369369e91e624825015d58012
Waltonr <waltonr18126@gmail.com>
2022-04-06 15:05:17 -0400

    init

---
+++ main.py
@@ -0,0 +1,4 @@
+from rpsls_game import RLPSLS_Game
+
+RPSLS = RLPSLS_Game()
+RPSLS.run_game()
---
+++ rpsls_game.py
@@ -0,0 +1,19 @@
+class RLPSLS_Game:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        self.welcome_message()
+        self.battle_phase()
+        self.display_winner()
+
+    def welcome_message(self):
+        print('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
+        user_input = input('How many players? 1 or 2: ')
+         
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
